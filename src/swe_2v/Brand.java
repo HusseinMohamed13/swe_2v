ddcaa99fe05641d1752b7639c02b90310a049eb6
Colochenero <47568654+Colochenero@users.noreply.github.com>
2019-12-30 16:43:36 +0200

    Update Brand.java

--- src/swe_2v/Brand.java
+++ src/swe_2v/Brand.java
@@ -3,24 +3,29 @@ package swe_2v;
 public class Brand {
 	String Name;
     String Category;
-    
+    //default constructor
     public Brand() {
    	  Name="";
    	  Category="";
     }
+	//constructor to to brand
     public Brand(String name,String category) {
    	  Name=name;
    	  Category=category;
     }
+	//set function
     public void setName(String name) {
 		Name = name;
 	 }
+	//get nam function
 	 public String getName() {
 		return Name;
 	 }
+	//set category function
 	 public void setCategory(String category) {
 		Category = category;
 	 }
+	//get category function
 	 public String getCategory() {
 		return Category;
 	 }
